bb1a55e898dda2e0e1550205130223508567fb08
Akin Karadag <akin.karadag@outlook.com>
2026-01-18 22:31:27 +0100

    Refactor Order and Tour classes; rename tour class to follow PascalCase convention and update tourManager functions for clarity

--- src/tourManager.js
+++ src/tourManager.js
@@ -3,9 +3,17 @@ const Tour = require('./tour');
 const tours = [];
 
 function createTour({tourId, vehicle, date, startLocation, endLocation}) {
-    const newTour = new Tour(tourId, vehicle, date, startLocation, endLocation);
-    tours.push(newTour);
-    return newTour;
+    const tour = new Tour(tourId, vehicle, date, startLocation, endLocation);
+    tours.push(tour);
+    return tour;
+}
+
+function addOrderToTour(tour, order){
+    tour.addOrder(order)
+}
+
+function loadOrderToVehicle(vehicle, order) {
+    order.getCargos().forEach(cargo => vehicle.loadCargo(cargo))
 }
 
 function getTours() {
