3006a206c148c49a9be5b00d748e53243c40869f
Akin Karadag <akin.karadag@outlook.com>
2026-01-18 15:56:11 +0100

    Add Account, CustomerProfile, and EmployeeProfile classes; remove Employee class

---
+++ src/account.js
@@ -0,0 +1,7 @@
+class Account{
+    constructor({id, email, password}){
+        this.id = id
+        this.email = email
+        this.password = password
+    }
+}
---
+++ src/employeeProfile.js
@@ -0,0 +1,8 @@
+class EmployeeProfile{
+    constructor({id, accountId, companyId, role}){
+        this.id = id
+        this.accountId = accountId
+        this.companyId = companyId
+        this.role = role
+    }
+}
