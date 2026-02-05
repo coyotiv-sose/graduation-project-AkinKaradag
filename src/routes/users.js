a858dd441b9a868645ebeb19b97cc0242a7bca60
AkinKaradag <122228927+AkinKaradag@users.noreply.github.com>
2026-02-05 13:03:27 +0000

    Add account routes with GET and POST

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,20 +1,15 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send([
-    {name: 'John Doe'},
-    {name: 'Jane Doe'},
-    {name: 'John Smith'},
-    {name: 'Jane Smith'}
-  ]) 
-});
-
-/* Create a new user */
-router.post('/', function(req, res, next){
-  const user = new User(req.body.name)
-  res.send(user)
+    res.send([{ name: 'John Doe' }, { name: 'Jane Doe' }, { name: 'John Smith' }, { name: 'Jane Smith' }])
 })
 
-module.exports = router;
+/* Create a new user */
+router.post('/', function(req, res, next) {
+    const user = new User(req.body.name)
+    res.send(user)
+})
+
+module.exports = router
