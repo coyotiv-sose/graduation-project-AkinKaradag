ec01ea636d0792f37dfec03dcdd6c021fcd7423c
AkinKaradag <122228927+AkinKaradag@users.noreply.github.com>
2026-02-16 13:06:00 +0000

    Fix and refactor code according to review
    - Add axios baseURL
    - Delete Order function in customer.js
    - Fix async/await in vehicle.js
    - Export employees.js
    - Change var to const

--- src/routes/companies.js
+++ src/routes/companies.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const router = express.Router()
 const LogisticCompany = require('../models/logistic-company')
 const customerManager = require('../managers/customer-manager')
@@ -8,43 +7,43 @@ const orderManager = require('../managers/order-manager')
 const vehicleManager = require('../managers/vehicle-manager')
 const tourManager = require('../managers/tour-manager')
 
-router.post('/', async (req, res, next) => {
-  try {
-    const company = await LogisticCompany.create(req.body)
-    res.status(201).json(company)
-  } catch (error) {
-    res.status(400).json({ error: error.message })
-  }
+router.post('/', async(req, res, next) => {
+    try {
+        const company = await LogisticCompany.create(req.body)
+        res.status(201).json(company)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 })
 
-router.get('/', async (req, res, next) => {
-  try {
-    const companies = await LogisticCompany.find()
-    res.status(200).json(companies)
-  } catch (error) {
-    res.status(500).json({ error: error.message })
-  }
+router.get('/', async(req, res, next) => {
+    try {
+        const companies = await LogisticCompany.find()
+        res.status(200).json(companies)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 })
 
-router.post('/:companyId/customers', async (req, res, next) => {
-  try {
-    const customer = await customerManager.createCustomer({
-      ...req.body,
-      company: req.params.companyId,
-    })
-    res.status(201).json(customer)
-  } catch (error) {
-    res.status(400).json({ error: error.message })
-  }
+router.post('/:companyId/customers', async(req, res, next) => {
+    try {
+        const customer = await customerManager.createCustomer({
+            ...req.body,
+            company: req.params.companyId,
+        })
+        res.status(201).json(customer)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 })
 
-router.get('/:companyId/customers', async (req, res, next) => {
-  try {
-    const customers = await customerManager.getCustomerByCompany(req.params.companyId)
-    res.status(200).json(customers)
-  } catch (error) {
-    res.status(500).json({ error: error.message })
-  }
+router.get('/:companyId/customers', async(req, res, next) => {
+    try {
+        const customers = await customerManager.getCustomerByCompany(req.params.companyId)
+        res.status(200).json(customers)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 })
 
 router.post('/:companyId/employees', async (req, res, next) => {
@@ -108,16 +107,16 @@ router.get('/:companyId/customers/:customerId/orders', async (req, res, next) =>
   }
 })
 
-router.post('/:companyId/vehicles', async (req, res, next) => {
-  try {
-    const newVehicle = await vehicleManager.createVehicle({
-      ...req.body,
-      company: req.params.companyId,
-    })
-    res.status(201).json(newVehicle)
-  } catch (error) {
-    res.status(400).json({ error: error.message })
-  }
+router.post('/:companyId/vehicles', async(req, res, next) => {
+    try {
+        const newVehicle = await vehicleManager.createVehicle({
+            ...req.body,
+            company: req.params.companyId,
+        })
+        res.status(201).json(newVehicle)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 })
 
 router.get('/:companyId/vehicles', async (req, res, next) => {
@@ -129,16 +128,16 @@ router.get('/:companyId/vehicles', async (req, res, next) => {
   }
 })
 
-router.post('/:companyId/tours', async (req, res, next) => {
-  try {
-    const newTour = await tourManager.createTour({
-      ...req.body,
-      company: req.params.companyId,
-    })
-    res.status(201).json(newTour)
-  } catch (error) {
-    res.status(400).json({ error: error.message })
-  }
+router.post('/:companyId/tours', async(req, res, next) => {
+    try {
+        const newTour = await tourManager.createTour({
+            ...req.body,
+            company: req.params.companyId,
+        })
+        res.status(201).json(newTour)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 })
 
 router.get('/:companyId/tours', async (req, res, next) => {
