02ac092c8727b20e5ec1e25acddb37dc7c56e839
AkinKaradag <122228927+AkinKaradag@users.noreply.github.com>
2026-02-17 15:20:57 +0000

    Add assign vehicle route and update test script

--- src/routes/tours.js
+++ src/routes/tours.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const tourManager = require('../managers/tour-manager')
+const vehicleManager = require('../managers/vehicle-manager')
 
 router.get('/:tourId', async(req, res, next) => {
     try {
@@ -17,6 +18,20 @@ router.get('/:tourId/cargos', async(req, res, next) => {
         res.status(200).json(cargos)
     } catch (error) {
         res.status(500).json({ error: error.message })
+    }
+})
+
+router.put('/:tourId/assign-vehicles', async(req, res, next) => {
+    try {
+        const tour = await tourManager.findTourById(req.params.tourId)
+        const vehicle = await vehicleManager.findVehicleById(req.body.vehicleId)
+        if (vehicle.status !== 'AVAILABLE') {
+            throw new Error('Vehicle is not available')
+        }
+        await tour.assignVehicle(vehicle)
+        res.status(200).json(tour)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
     }
 })
 
