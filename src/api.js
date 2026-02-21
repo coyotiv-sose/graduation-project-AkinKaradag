cd065e17062249b2535475e9f2a680824cb719a9
Akin Karadag <akin.karadag@outlook.com>
2026-02-21 23:06:19 +0100

    Add test service to docker compose

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+  baseURL: process.env.BASE_URL || 'http://localhost:3000',
 })
 
 module.exports = api
