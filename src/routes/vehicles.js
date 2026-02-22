fecc0838eb6bc832202331c2518aca7da763011d
Akin Karadag <akin.karadag@outlook.com>
2026-02-22 13:46:14 +0100

    Change URL for vehicle assignee to follow REST principles

--- src/routes/vehicles.js
+++ src/routes/vehicles.js
@@ -1,14 +1,15 @@
 const express = require('express')
+
 const router = express.Router()
 const vehicleManager = require('../managers/vehicle-manager')
 
-router.get('/:vehicleId', async(req, res, next) => {
-    try {
-        const vehicle = await vehicleManager.findVehicleById(req.params.vehicleId)
-        res.status(200).json(vehicle)
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
+router.get('/:vehicleId', async (req, res, next) => {
+  try {
+    const vehicle = await vehicleManager.findVehicleById(req.params.vehicleId)
+    res.status(200).json(vehicle)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
 })
 
 module.exports = router
