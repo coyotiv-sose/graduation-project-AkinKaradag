64d7efed302b5181ad4fd579880266ec7a7d0488
Akin Karadag <akin.karadag@outlook.com>
2026-02-22 13:46:45 +0100

    Change URL for vehicle assignee to follow REST principles

--- src/managers/tour-manager.js
+++ src/managers/tour-manager.js
@@ -2,21 +2,21 @@ const { findOrderById } = require('./order-manager')
 const Tour = require('../models/tour')
 
 const createTour = async tourData => {
-    return Tour.create(tourData)
+  return Tour.create(tourData)
 }
 
 const findTourById = async tourId => {
-    const tour = await Tour.findById(tourId)
-    if (!tour) throw new Error('Tour not found')
-    return tour
+  const tour = await Tour.findById(tourId)
+  if (!tour) throw new Error('Tour not found')
+  return tour
 }
 
 const getAllToursByCompany = async companyId => Tour.find({ company: companyId })
 
 const getCargosByTour = async tourId => {
-    const tour = await Tour.findById(tourId)
-    if (!tour) throw new Error('Tour not found')
-    return tour.orders.flatMap(order => order.cargos)
+  const tour = await Tour.findById(tourId)
+  if (!tour) throw new Error('Tour not found')
+  return tour.orders.flatMap(order => order.cargos)
 }
 
 module.exports = { createTour, findTourById, getAllToursByCompany, getCargosByTour }
