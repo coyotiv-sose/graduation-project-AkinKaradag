47610d5ff6ea8c903e296b21b8b6f6ffcbd6e187
Akin Karadag <akin.karadag@outlook.com>
2026-02-23 22:30:06 +0100

    Refactor code formatting for consistency

--- src/models/logistic-company.js
+++ src/models/logistic-company.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const logisticCompanySchema = new mongoose.Schema({
-    companyName: { type: String, required: true },
-    address: { type: String, required: true },
-    postalCode: { type: String, required: true },
-    city: { type: String, required: true },
+  companyName: { type: String, required: true },
+  address: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  city: { type: String, required: true },
 })
 
 module.exports = mongoose.model('LogisticCompany', logisticCompanySchema)
--- src/models/tour.js
+++ src/models/tour.js
@@ -1,35 +1,35 @@
 const mongoose = require('mongoose')
-const orderSchema = require('./order')
 const autopopulate = require('mongoose-autopopulate')
+const orderSchema = require('./order')
 
 const tourSchema = new mongoose.Schema({
-    company: { type: mongoose.Schema.Types.ObjectId, ref: 'LogisticCompany', required: true},
-    vehicle: { type: mongoose.Schema.Types.ObjectId, ref: 'Vehicle', default: null },
-    date: { type: Date, required: true },
-    startLocation: { type: String, required: true },
-    endLocation: { type: String, required: true },
-    orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
-    state: { type: String, enum: ['STARTED', 'PLANNED', 'CANCELLED', 'FINISHED'] },
+  company: { type: mongoose.Schema.Types.ObjectId, ref: 'LogisticCompany', required: true },
+  vehicle: { type: mongoose.Schema.Types.ObjectId, ref: 'Vehicle', default: null },
+  date: { type: Date, required: true },
+  startLocation: { type: String, required: true },
+  endLocation: { type: String, required: true },
+  orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
+  state: { type: String, enum: ['STARTED', 'PLANNED', 'CANCELLED', 'FINISHED'] },
 })
 
-tourSchema.methods.addOrder = function(order) {
-    this.orders.push(order._id)
-    return this.save()
+tourSchema.methods.addOrder = function (order) {
+  this.orders.push(order._id)
+  return this.save()
 }
 
-tourSchema.methods.assignVehicle = function(vehicle) {
-    this.vehicle = vehicle._id
-    return this.save()
+tourSchema.methods.assignVehicle = function (vehicle) {
+  this.vehicle = vehicle._id
+  return this.save()
 }
 
-tourSchema.methods.startTour = function() {
-    this.state = 'STARTED'
-    return this.save()
+tourSchema.methods.startTour = function () {
+  this.state = 'STARTED'
+  return this.save()
 }
 
-tourSchema.methods.endTour = function() {
-    this.state = 'FINISHED'
-    return this.save()
+tourSchema.methods.endTour = function () {
+  this.state = 'FINISHED'
+  return this.save()
 }
 
 tourSchema.plugin(autopopulate)
