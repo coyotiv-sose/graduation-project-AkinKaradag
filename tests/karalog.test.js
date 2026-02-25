1727132d1c55e746c242d9ecc73f7e95d66e92c5
Akin Karadag <akin.karadag@outlook.com>
2026-02-25 22:56:02 +0100

    Add some test for my learning process

--- tests/karalog.test.js
+++ tests/karalog.test.js
@@ -4,10 +4,12 @@ const request = require('supertest')
 
 const app = require('../src/app')
 const Company = require('../src/models/logistic-company')
+const Vehicle = require('../src/models/vehicle')
 
 describe('Karalog', () => {
     beforeEach(async () => {
         await Company.deleteMany()
+        await Vehicle.deleteMany()
     })
   it('can be create a company', async () => {
     const companyData = {
@@ -23,6 +25,23 @@ describe('Karalog', () => {
         .post('/companies')
         .send(expectedOutput)
 
+    expect(actualOutput.body).toMatchObject(expectedOutput.body)
+  })
+  it('shoul be create a vehicle', async () => {
+    const vehicleData = {
+        name: 'Truck1',
+        brand: 'Mercedes',
+        model: 'Sprinter',
+        year: 2005,
+        payLoad: 800,
+    }
+    const expectedOutput = {
+        ...vehicleData,
+    }
+    const actualOutput = await request(app)
+        .post('/vehicles')
+        .send(expectedOutput)
+    
     expect(actualOutput.body).toMatchObject(expectedOutput.body)
   })
 })
