aae1c00b474b5e2a13f143bb26c1f56bac0c4959
Akin Karadag <akin.karadag@outlook.com>
2026-03-01 20:21:20 +0100

    Add tests for updating vehicle state and handling non-existent vehicles

--- tests/vehicle.test.js
+++ tests/vehicle.test.js
@@ -46,4 +46,31 @@ describe('Vehicle', () => {
         expect(response.status).toBe(500)
         expect(response.body.error).toBe('Vehicle not found')
     })
+
+    it('should change the state of vehicle', async() => {
+
+        const vehicleResponse = await request(app).post(`/companies/${company.body._id}/vehicles`).send({
+            name: 'Truck3',
+            brand: 'Mercedes',
+            model: 'Sprinter',
+            year: 2005,
+            payLoad: 800,
+        })
+
+        const response = await request(app).put(`/companies/${company.body._id}/vehicles/${vehicleResponse.body._id}`).send({
+            state: 'IN_GARAGE'
+        })
+        expect(response.status).toBe(200)
+        expect(response.body.state).toBe('IN_GARAGE')
+    })
+
+    it('should not find vehicle when trying to update vehicle and it does not exist', async() => {
+        const fakeId = new mongoose.Types.ObjectId()
+        const response = await request(app).put(`/companies/${company.body._id}/vehicles/${fakeId}`).send({
+            state: 'IN_GARAGE'
+        })
+        expect(response.status).toBe(404)
+        expect(response.body.error).toBe('Vehicle not found')
+    })
+
 })
