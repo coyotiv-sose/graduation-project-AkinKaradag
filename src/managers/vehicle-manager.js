eb2b22e654b048bdecb78fa365995fd3526fa6b1
Akin Karadag <akin.karadag@outlook.com>
2026-03-01 20:19:54 +0100

    Add updateVehicle function to manage vehicle updates

--- src/managers/vehicle-manager.js
+++ src/managers/vehicle-manager.js
@@ -11,6 +11,20 @@ const findVehicleById = async vehicleId => {
   return vehicle
 }
 
+const updateVehicle = async (vehicleId, updatedData) => {
+  const vehicle = await Vehicle.findOneAndUpdate(
+    { _id: vehicleId },
+    { $set: updatedData },
+    { new: true, runValidators: true }
+  )
+
+  if (!vehicle) {
+    const exist = await Vehicle.findById(vehicleId)
+    if (!exist) throw new Error('Vehicle not found')
+  }
+  return vehicle
+}
+
 const getAllVehiclesOfCompany = companyId => Vehicle.find({ company: companyId })
 
-module.exports = { createVehicle, findVehicleById, getAllVehiclesOfCompany }
+module.exports = { createVehicle, findVehicleById, getAllVehiclesOfCompany, updateVehicle }
