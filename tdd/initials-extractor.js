20a72cd00129da3b02c5cc9696320bdc42f8ef7d
AkinKaradag <122228927+AkinKaradag@users.noreply.github.com>
2026-03-04 14:57:34 +0000

    Create a Backend folder and move all backend related files into it

--- tdd/initials-extractor.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(name) {
-    const names = name.split(' ')
-    return names
-        .filter(n => n[0].toUpperCase() == n[0] || n == 'zu')
-        .map(n => n[0])
-        .join('')
-}
