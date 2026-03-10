575495de61be2fc5efbf1e88d17d6b4962d1ce0b
Akin Karadag <akin.karadag@outlook.com>
2026-03-10 08:16:28 +0100

    WIP: vue learning experiments

---
+++ frontend/src/components/Counter.vue
@@ -0,0 +1,31 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+
+import { ref } from 'vue'
+
+defineProps({
+    name: {
+        type: String,
+        required: true
+    }
+})
+
+const count = ref(0)
+
+const increment = () => {
+    count.value++
+}
+
+const decrement = () => {
+    count.value--
+}
+
+</script>
+
+<template lang="pug">
+    div
+    h1 {{ name }}
+    p Count: {{ count }}
+    button(@click='increment') Increment
+    button(@click='decrement') Decrement
+</template>
