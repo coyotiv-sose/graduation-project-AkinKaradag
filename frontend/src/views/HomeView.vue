83e4f075eb8f2de8fbc7a98a4cafd0f88665f0a9
AkinKaradag <122228927+AkinKaradag@users.noreply.github.com>
2026-03-10 15:26:19 +0000

    Add pinia for global state of Counter Options and Composition API

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,14 +1,13 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
 import Counter from '../components/Counter.vue'
-import CounterOptionsApi
- from '@/components/CounterOptionsApi.vue';
+import CounterOptionsApi from '@/components/CounterOptionsApi.vue'
 </script>
 
 <template>
   <main>
-    <Counter name="Beer"/>
-    <CounterOptionsApi name="Wine"/>
+    <Counter name="Beer" />
+    <CounterOptionsApi name="Wine" />
     <Suspense>
       <TheWelcome />
     </Suspense>
