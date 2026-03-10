5e8dd4254fc21462ccabe9bd8abd27faa6030932
AkinKaradag <122228927+AkinKaradag@users.noreply.github.com>
2026-03-10 16:34:38 +0000

    Add a new Counter for experiment with Composition and Options API

---
+++ frontend/src/components/Counter3.vue
@@ -0,0 +1,31 @@
+<script>
+import { useCounterCompositionStore } from '@/stores/counterComposition'
+import { mapActions, mapState } from 'pinia'
+
+export default {
+  name: 'Counter',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    ...mapState(useCounterCompositionStore, ['count', 'doubleCount']),
+  },
+
+  methods: {
+    ...mapActions(useCounterCompositionStore, ['increment', 'decrement']),
+  },
+}
+</script>
+
+<template lang="pug">
+div
+    h1 {{ name }}
+    p Count: {{ count }}
+    p DoubleCount: {{ doubleCount }}
+    button(@click='increment') Increment
+    button(@click='decrement') Decrement
+</template>
