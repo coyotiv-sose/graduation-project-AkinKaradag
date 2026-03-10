def0e8ea7c098a179c2872efcd27f9e8cd770718
Akin Karadag <akin.karadag@outlook.com>
2026-03-10 08:16:28 +0100

    Create Counter with Options and Composition API

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import Counter from '../components/Counter.vue'
+import CounterOptionsApi
+ from '@/components/CounterOptionsApi.vue';
 </script>
 
 <template>
   <main>
+    <Counter name="Beer"/>
+    <CounterOptionsApi name="Wine"/>
     <Suspense>
       <TheWelcome />
     </Suspense>
