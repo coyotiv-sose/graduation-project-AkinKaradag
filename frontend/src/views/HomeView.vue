2409b38fd1b0816fb8475ba18f29eed21934408c
Akin Karadag <akin.karadag@outlook.com>
2026-03-12 00:15:45 +0100

    Remove unused counter components and related store files

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,17 +1,10 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
-import Counter from '../components/Counter.vue'
-import CounterOptionsApi from '@/components/CounterOptionsApi.vue'
-import Counter2 from '@/components/Counter2.vue'
-import Counter3 from '@/components/Counter3.vue'
+
 </script>
 
 <template>
   <main>
-    <Counter name="Beer" />
-    <CounterOptionsApi name="Wine" />
-    <Counter2 name="Vodka" />
-    <Counter3 name="Raki" />
     <Suspense>
       <TheWelcome />
     </Suspense>
