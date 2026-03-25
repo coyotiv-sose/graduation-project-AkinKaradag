9a55990b2e06f595c7c0255bbb49b49d82dcf50e
Akin Karadag <akin.karadag@outlook.com>
2026-03-25 22:22:38 +0100

    Add login route and link in the navigation menu

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,33 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import LoginView from '../views/LoginView.vue'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/companies',
-      name: 'company',
-      component: () => import('../views/CompanyView.vue'),
-    },
-    {
-      path: '/customers',
-      name: 'customer',
-      component: () => import('../views/CustomerView.vue')
-    }
-  ],
+    history: createWebHistory(
+        import.meta.env.BASE_URL),
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: HomeView,
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () =>
+                import ('../views/AboutView.vue'),
+        },
+        {
+            path: '/companies',
+            name: 'company',
+            component: () =>
+                import ('../views/CompanyView.vue'),
+        },
+        {
+            path: '/customers',
+            name: 'customer',
+            component: () =>
+                import ('../views/CustomerView.vue')
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView,
+        }
+    ],
 })
 
 export default router
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'LoginView',
+    data() {
+        return {
+            email: '',
+            password: '',
+        }
+    },
+    methods: {
+        async login() {
+            await axios.post('/accounts/session', { email: this.email, password: this.password })
+        }
+    }
+}
+</script>
+
+<template lang="pug">
+h1 Login
+form(@submit.prevent='login')
+    div
+        input(v-model='email' placeholder='Email')
+    div
+        input(v-model='password' type='password' placeholder='Password')
+    div
+        button(type='submit') Login
+</template> 
