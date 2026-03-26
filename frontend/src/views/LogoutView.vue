600bcb49bd67b30ce30647a1e7fb1304cc1ff58d
AkinKaradag <122228927+AkinKaradag@users.noreply.github.com>
2026-03-26 12:43:52 +0000

    Implement user authentication with login, registration, and logout functionality

---
+++ frontend/src/views/LogoutView.vue
@@ -0,0 +1,16 @@
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'LogoutView',
+  async mounted() {
+    await axios.delete('/accounts/session')
+    this.$root.user = null
+    this.$router.push('/')
+  },
+}
+</script>
+
+<template lang="pug">
+p Logging out...
+</template>
