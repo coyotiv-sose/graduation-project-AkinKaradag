6f734fbffc07b281262f204848aef93e17a12cd2
AkinKaradag <122228927+AkinKaradag@users.noreply.github.com>
2026-03-31 18:04:08 +0000

    Refactor HomeView and App components for improved navigation and layout

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
 const router = createRouter({
     history: createWebHistory(
         import.meta.env.BASE_URL),
@@ -11,25 +10,28 @@ const router = createRouter({
             component: HomeView,
         },
         {
-            path: '/about',
-            name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import ('../views/AboutView.vue'),
-        },
-        {
             path: '/companies',
-            name: 'company',
+            name: 'companies',
             component: () =>
                 import ('../views/CompanyView.vue'),
         },
         {
-            path: '/customers',
-            name: 'customer',
+            path: '/companies/:companyId',
+            name: 'companyDetail',
+            component: () =>
+                import ('../views/CompanyDetailView.vue'),
+        },
+        {
+            path: '/companies/:companyId/customers',
+            name: 'companyCustomers',
             component: () =>
                 import ('../views/CustomerView.vue'),
+        },
+        {
+            path: '/companies/:companyId/employees',
+            name: 'companyEmployees',
+            component: () =>
+                import ('../views/EmployeeView.vue'),
         },
         {
             path: '/orders',
@@ -41,11 +43,6 @@ const router = createRouter({
             path: '/login',
             name: 'login',
             component: LoginView,
-        },
-        {
-            path: '/register',
-            name: 'register',
-            component: RegisterView,
         },
         {
             path: '/logout',
