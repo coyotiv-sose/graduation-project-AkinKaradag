7ff94166fc859c846872c3c2a189c0f394f7df94
AkinKaradag <122228927+AkinKaradag@users.noreply.github.com>
2026-03-31 18:02:56 +0000

    Refactor Company component and add CompanyDetailView for detailed company information

--- frontend/src/components/Company.vue
+++ frontend/src/components/Company.vue
@@ -1,56 +1,23 @@
 <script>
-import { useCompanyStore } from '@/stores/companyStore';
-import CreateFormWrapper from './CreateFormWrapper.vue'
+import { useCompanyStore } from '@/stores/companyStore'
 
 export default {
-  name: 'CompanyForm',
-  components: { CreateFormWrapper },
-  data() {
-    return {
-      companyName: '',
-      address: '',
-      postalCode: '',
-      city: '',
-    }
-  },
-
+  name: 'CompanyList',
   computed: {
     companies() {
       return useCompanyStore().companies
-    }
+    },
   },
-
   async mounted() {
     await useCompanyStore().getAllCompanies()
-  },
-
-  methods: {
-    async submitCompany() {
-      await useCompanyStore().createCompany({
-        companyName: this.companyName,
-        address: this.address,
-        postalCode: this.postalCode,
-        city: this.city,
-      })
-      this.companyName = ''
-      this.address = ''
-      this.postalCode = ''
-      this.city = ''
-    },
   },
 }
 </script>
 
 <template lang="pug">
-CreateFormWrapper(:onSubmit='submitCompany', submitLabel='Create company')
-  input(v-model='companyName' placeholder='Company Name')
-  input(v-model='address' placeholder='Address')
-  input(v-model='postalCode' placeholder='PostalCode')
-  input(v-model='city' placeholder='City')
-
 h2 Companies
 ul
-   li(v-for='company in companies' :key='company._id')
-    | {{ company.companyName }} - {{ company.city }}
-
+  li(v-for='company in companies' :key='company._id')
+    router-link(:to='`/companies/${company._id}`')
+      | {{ company.companyName }} - {{ company.city }}
 </template>
