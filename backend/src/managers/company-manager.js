3d3776f2150d0db8a9e751659221b06f6943df29
Akin Karadag <akin.karadag@outlook.com>
2026-04-18 02:26:35 +0200

    Implement company management functions in company-manager.js

---
+++ backend/src/managers/company-manager.js
@@ -0,0 +1,38 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+const Company = require('../models/company')
+
+const createCompany = async companyData => {
+  const company = await Company.create(companyData)
+  return company
+}
+
+const getCompanyById = async companyId => {
+  const company = await Company.findById(companyId)
+  if (!company) throw new Error('Company not found')
+  return company
+}
+
+const getAllCompanies = () => Company.find()
+
+const updateCompany = async (companyId, updates) => {
+  const company = await Company.findById(companyId)
+  if (!company) throw new Error('Company not found')
+  Object.assign(company, updates)
+  await company.save()
+  return company
+}
+
+const deleteCompany = async companyId => {
+  const company = await Company.findById(companyId)
+  if (!company) throw new Error('Company not found')
+  await Company.findByIdAndDelete(companyId)
+}
+
+module.exports = {
+  createCompany,
+  getCompanyById,
+  getAllCompanies,
+  updateCompany,
+  deleteCompany,
+}
