e06a42c027317055be8b228d54774f3da0a0d279
Akin Karadag <akin.karadag@outlook.com>
2026-04-22 20:47:15 +0200

    Remove conditional inclusion of Vue DevTools from Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [
     vue(),
-    ...(mode === 'development' ? [vueDevTools()] : []),
+    // vueDevTools(),
   ],
   server: {
     allowedHosts: true,
@@ -18,4 +18,4 @@ export default defineConfig(({ mode }) => ({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-}))
+})
