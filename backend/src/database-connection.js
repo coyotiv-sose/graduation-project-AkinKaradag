645ef436b768df2641dafebd0215895d945dee10
Akin Karadag <akin.karadag@outlook.com>
2026-05-09 01:46:11 +0200

    Improve error handling for MongoDB connection

--- backend/src/database-connection.js
+++ backend/src/database-connection.js
@@ -3,4 +3,7 @@ const mongoose = require('mongoose')
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING)
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log('Could not connect to MongoDB', err))
+  .catch(err => {
+    console.error('Could not connect to MongoDB', err)
+    process.exit(1)
+  })
